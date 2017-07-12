dc79f8939aafeb63347fc248650e0cfcd6a226ff
Matt Sollie <matt.sollie@gcu.edu>
2017-07-12 15:46:05 -0700

    added examples

---
+++ Examples/cDSCDockerSwarmExample.ps1
@@ -0,0 +1,56 @@
+configuration TestDockerSwarm
+{
+
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    Import-DscResource -ModuleName cDSCDockerSwarm
+    node ("localhost")
+    {
+     WindowsFeature RSatAD
+        {
+            Ensure = "Present"
+            Name   = "RSAT-AD-Powershell"
+        }
+
+         WindowsFeature ContainerInstall
+        {
+            Ensure = "Present"
+            Name   = "Containers"
+        }
+
+       cDockerBinaries Docker
+       {
+            Ensure = 'Present'
+            DependsOn = '[WindowsFeature]ContainerInstall'
+            version = '17.06.0-ce'
+            DownloadChannel = 'Stable'
+       }
+
+       cDockerSwarm Swarm {
+            DependsOn = '[cDockerBinaries]Docker'
+            SwarmMasterURI = '10.10.255.255:2377'
+            SwarmMode = 'Active'
+            ManagerCount = 3
+            SwarmManagement = 'Automatic'
+       } 
+
+       cInsecureRegistryCert registry
+       {
+            Ensure = 'Present'
+            DependsOn = '[cDockerBinaries]Docker'
+            registryURI = 'myregistry:5000'
+            Certificate = '-----BEGIN CERTIFICATE-----
+            -----------------------------------
+-----END CERTIFICATE-----'
+       }
+
+       cDockerConfig test
+       {
+            Ensure = 'Present'
+            DependsOn = '[cDockerBinaries]Docker'
+            RestartOnChange = $true
+            ExposeAPI = $true
+            InsecureRegistries = 'myregistry:5000'
+            Labels = "my.environment=test", "my.winver=core"
+        }
+    }
+}
